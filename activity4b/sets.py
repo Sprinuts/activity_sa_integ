547adfda62be470e5a58a7e559816c96285ce47e
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-02-03 13:05:06 +0800

    Add files via upload

---
+++ activity4b/sets.py
@@ -0,0 +1,19 @@
+setA = {"a","g", "d", "f", "c", "b"}
+setB = {"l","m","o","b","c","h"}
+setC = {"k", "i", "j", "f", "d", "c", "h"}
+
+# print(setB.difference(setA).difference(setC))
+
+# print(setA.intersection(setB).intersection(setC))
+
+# print(setA.intersection(setC).difference(setB))
+
+# print(setA.union(setC).intersection(setB))
+
+# print(setC.intersection(setA))
+
+# print(setC.difference(setA))
+
+# print("There are",len(setA.union(setB)), "elements in setA and setB")
+
+# print("There are",len(setB.difference(setA).difference(setC)),"elements in setB but not in setA and setC")
